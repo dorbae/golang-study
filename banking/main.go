964422bb7c2db9de5e486461d098441ffe92609a
Seongbae Chang <dorbae.io@gmail.com>
2020-10-02 19:46:23 +0900

    Banking Project done
    Banking Project done

--- banking/main.go
+++ banking/main.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/dorbae/banking/accounts"
+	"github.com/dorbae/golang-study/banking/accounts"
 )
 
 func main() {
+	// #2.0 Account + NewAccoutn
+	// https://nomadcoders.co/go-for-beginners/lectures/1512
 	// account := banking.Account{Owner: "dorbae", Balance: 1000}
 	// account := banking.Account{Owner: "dorbae"}
 	account := accounts.NewAccount("dorbae")
-
 	fmt.Println(account)
 
+	// #2.1 Methods part One
+	// https://nomadcoders.co/go-for-beginners/lectures/1513
 	account.Deposit(10)
 	fmt.Println(account.Balance())
 
-	err := account.Withdraw(20)
+	// #2.2 Methods part Two
+	// https://nomadcoders.co/go-for-beginners/lectures/1514
+	// err := account.Withdraw(20)
+	err := account.Withdraw(5)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	fmt.Println(account.Balance())
 
+	// #2.3 Finishig up
+	// https://nomadcoders.co/go-for-beginners/lectures/1515
+	fmt.Println(account) // Print class
 }
